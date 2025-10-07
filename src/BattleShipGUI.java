b500a5a4d19803b001c931e2c9fb3ff5c9096046
Santiago lagares <santiagolagares@MacBook-Pro-de-Santiago.local>
2025-10-07 13:53:17 -0300

    v1.0.1 (panttalla principal y funciones switch de vistas)

--- src/BattleShipGUI.java
+++ src/BattleShipGUI.java
@@ -5,31 +5,24 @@ public class BattleShipGUI extends JFrame {
 
     public BattleShipGUI() {
         setTitle("BattleShip");
+        setBounds(500,500,500,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout(20, 20)); //Gap entre paneles internos
 
 
+        //---Ventana Main---
+        JPanel panelPrincipal = new JPanel();
+        panelPrincipal.setOpaque(true);
+        JButton battleShip = new JButton("Jugar");
+        battleShip.setPreferredSize(new Dimension(100, 50));
+        panelPrincipal.add(battleShip, BorderLayout.CENTER);
 
-        //----Jugador 1----
-        JPanel panelTablero1 = crearTablero();
-        JPanel panelPowerUps1 = crearPanelPowerUps("pwu1", "pwu2", "pwu3");
-        JPanel panelJugador1 = new JPanel(new BorderLayout());
-        panelJugador1.add(panelTablero1, BorderLayout.NORTH);
-        panelJugador1.add(panelPowerUps1, BorderLayout.SOUTH);
+        battleShip.addActionListener(e -> {
+            switchPanel();
+            remove(panelPrincipal);
+        });
 
-        //----Jugador 2----
-        JPanel panelTablero2 = crearTablero();
-        JPanel panelPowerUps2 = crearPanelPowerUps("pwu4", "pwu5", "pwu6");
-        JPanel panelJugador2 = new JPanel(new BorderLayout());
-        panelJugador2.add(panelTablero2, BorderLayout.NORTH);
-        panelJugador2.add(panelPowerUps2, BorderLayout.SOUTH);
-
-        // ---- Agregar paneles a la ventana ----
-        add(panelJugador1, BorderLayout.WEST);
-        add(panelJugador2, BorderLayout.EAST);
-
-
-        pack();
+        add(panelPrincipal, BorderLayout.CENTER);
         setVisible(true);
     }
 
@@ -56,5 +49,26 @@ public class BattleShipGUI extends JFrame {
         return panel;
     }
 
+    //---Cambio Pantalla Juego---
 
+    public void switchPanel() {
+        //----Jugador 1----
+        JPanel panelTablero1 = crearTablero();
+        JPanel panelPowerUps1 = crearPanelPowerUps("pwu1", "pwu2", "pwu3");
+        JPanel panelJugador1 = new JPanel(new BorderLayout());
+        panelJugador1.add(panelTablero1, BorderLayout.NORTH);
+        panelJugador1.add(panelPowerUps1, BorderLayout.SOUTH);
+
+        //----Jugador 2----
+        JPanel panelTablero2 = crearTablero();
+        JPanel panelPowerUps2 = crearPanelPowerUps("pwu4", "pwu5", "pwu6");
+        JPanel panelJugador2 = new JPanel(new BorderLayout());
+        panelJugador2.add(panelTablero2, BorderLayout.NORTH);
+        panelJugador2.add(panelPowerUps2, BorderLayout.SOUTH);
+
+        // ---- Agregar paneles a la ventana ----
+        add(panelJugador1, BorderLayout.WEST);
+        add(panelJugador2, BorderLayout.EAST);
+        pack();
+    }
 }
